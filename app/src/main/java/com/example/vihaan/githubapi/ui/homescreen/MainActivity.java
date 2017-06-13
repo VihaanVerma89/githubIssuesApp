786ad291c9529aea91ce1d08774cf1dd7028aae4
Vihaan <vihaanverma21@gmail.com>
2017-06-13 23:58:31 +0530

    Done

--- app/src/main/java/com/example/vihaan/githubapi/ui/homescreen/MainActivity.java
+++ app/src/main/java/com/example/vihaan/githubapi/ui/homescreen/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 
 import com.example.vihaan.githubapi.R;
+import com.crashlytics.android.Crashlytics;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fabric.with(this, new Crashlytics());
         setContentView(R.layout.activity_main);
         showFragment();
     }
